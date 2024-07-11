eed7a5e11d62bdd865559f2aa24d61b3d114a4d7
Adi <anonymous2x4a@gmail.com>
2024-07-11 06:56:29 +0530

    Initial commit

---
+++ bot.php
@@ -0,0 +1,28 @@
+ECHO is on.
+<?php
+require 'vendor/autoload.php';
+
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Types\Update;
+
+// Replace 'YOUR_BOT_TOKEN' with your actual bot token
+$bot = new BotApi('7388917863:AAFseb7fG51tjGpjbW5gF7JMaTy25TkL-m4');
+
+$bot->on(function (Update $update) use ($bot) {
+    $message = $update->getMessage();
+    $chatId = $message->getChat()->getId();
+    $text = $message->getText();
+
+    if ($text == "/start") {
+        $bot->sendMessage($chatId, "Welcome to the Bot!");
+    } elseif ($text == "/hello") {
+        $bot->sendMessage($chatId, "Hello there!");
+    } else {
+        $bot->sendMessage($chatId, "I don't understand that command.");
+    }
+}, function () {
+    return true;
+});
+
+$bot->run();
+$bot->run(['polling' => true]);
